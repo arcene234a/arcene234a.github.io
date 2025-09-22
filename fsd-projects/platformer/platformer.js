3581eebfbda32b9193434a2b15b34b156d9d9ca3
arcene234a <arcene234@wearescpps.org>
2025-09-22 15:48:54 +0000

    updated

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,7 +27,7 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+     // toggleGrid();
 
 
     // TODO 2 - Create Platforms
@@ -50,7 +50,7 @@ createPlatform(900, 200, 50, 25, "white");
 createPlatform(950, 200, 50, 25, "white");
 createPlatform(1000, 200, 50, 25, "white");
 createPlatform(1050, 200, 50, 25, "white");
-createPlatform(750, 250, 50, 25, "white");
+createPlatform(775, 250, 50, 25, "white");
 createPlatform(520, 265, 50, 25, "white");
 createPlatform(200, 650, 50, 25, "white");
 createPlatform(420, 400, 200, 25, "white");
@@ -58,10 +58,13 @@ createPlatform(450, 265, 50, 25, "white");
 createPlatform(450, 125, 50, 25, "white");
 createPlatform(100, 175, 50, 25, "white");
 createPlatform(350, 175, 50, 15, "white");
+createPlatform(675, 175, 50, 15, "white");
+createPlatform(525, 115, 75, 15, "white");
 createPlatform(0, 100, 50, 50, "lime"); // bright green for a finished platform
     // TODO 3 - Create Collectables
 createCollectable("steve", 1350, 50);
 createCollectable("diamond", 450, 10, 0.5, 0.7);
+createCollectable("kennedi", 525, 75, 0.5, 0.7);
 
 
     
